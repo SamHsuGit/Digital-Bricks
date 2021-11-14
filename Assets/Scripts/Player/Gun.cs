bd687923b6b41fbe5f2b8e6e63181c16a9b5f3ad
SamHsuGit <hsu3.samuel@gmail.com>
2021-11-14 16:04:39 -0500

    Update Gun.cs

--- Assets/Scripts/Player/Gun.cs
+++ Assets/Scripts/Player/Gun.cs
@@ -162,6 +162,6 @@ public class Gun : NetworkBehaviour
     public void BreakBaseObPiece(int piece)
     {
         //gameObject.GetComponent<Health>().SpawnCopyRb(baseModelPieces[piece]);
-        Destroy(baseModelPieces[piece]);
+        baseModelPieces[piece].SetActive(false);
     }
 }
