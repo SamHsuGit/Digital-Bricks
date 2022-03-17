afbe199a2422d8d719dcc43715421e1bd43959b5
SamHsuGit <hsu3.samuel@gmail.com>
2022-03-17 09:48:42 -0400

    updated world and structure

--- Assets/Scripts/Proc Gen/Structure.cs
+++ Assets/Scripts/Proc Gen/Structure.cs
@@ -138,6 +138,7 @@ public static class Structure
     static Queue<VoxelMod> MakeBaseVBOImport(Vector3 position)
     {
         Queue<VoxelMod> queue = new Queue<VoxelMod>();
+        LDrawImportRuntime.Instance.CalcBaseObSize(LDrawImportRuntime.Instance.baseOb);
         int xRadius = LDrawImportRuntime.Instance.baseObSizeX / 2;
         int zRadius = LDrawImportRuntime.Instance.baseObSizeZ / 2;
         xRadius += 1; // safety boundary in case import is offset by 1 block
