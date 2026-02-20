84c87d5192179b3bda6cbd56e0be224d42979a86
Sam Hsu <hsu3.samuel@gmail.com>
2026-02-20 16:27:00 -0500

    added crafting script

---
+++ Assets/Scripts/UI/Crafting.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Crafting : MonoBehaviour
+{
+
+    
+    // add functions to be called here that take items as input and return what item to output
+    // add functionality that can craft multiple items at once?
+}
